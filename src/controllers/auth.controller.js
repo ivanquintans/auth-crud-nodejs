2c732022bed4d4ce565e3889eaab1ae394bd1cd7
ivanquintans_ <ivanqg21@gmail.com>
2024-07-19 19:34:58 +0200

    hasheo realizado

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,14 +1,28 @@
 import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs'
 
 export const register = async (req, res) => {
     const {email, password , username} = req.body;
     
     try {
-        const newUser = new User({username, email, password});
-        console.log(newUser);
+
+        //hasheamos la contraseña antes de almacenarla
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({
+            username, 
+            email, 
+            password: hashedPassword,
+        });
+
         //guardamos el nuevo usuario para tener el timestamp
         const savedUser = await newUser.save();	
-        res.json(savedUser);
+        res.json({
+            id: savedUser.id,
+            username: savedUser.username,
+            email: savedUser.email,
+            createdAt: savedUser.createdAt,
+            updatedAt: savedUser.updatedAt,
+        });
         
     } catch (error) {
         console.log(error);
