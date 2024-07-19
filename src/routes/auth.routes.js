9be7c655cc6735d25043c994d0d71bd6d9793989
ivanquintans_ <ivanqg21@gmail.com>
2024-07-19 15:38:18 +0200

    realizando las primeras peticiones

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import { register, login } from '../controllers/auth.controller.js'
 
 const router = Router();
 
-router.post('/register')
-router.post('/login')
+router.post('/register', register)
+router.post('/login' , login);	
+
+export default router;
